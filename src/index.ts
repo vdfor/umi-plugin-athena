b96208200f5cf6d26c7b705602b5a9b04086d1d6
vdfor <jiakun.li@qq.com>
2022-05-16 18:51:21 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { IApi } from '@umijs/types';
+
+
+export default (api: IApi) => {
+  api.describe({
+    key: 'athena',
+    config: {
+      default: {
+        polyfill: undefined,
+        entryCodeAhead: '',
+      },
+      schema(joi: any) {
+        const itemSchema = joi.object({
+          polyfill: joi.string(),
+          entryCodeAhead: joi.string(),
+        });
+        return itemSchema;
+      },
+    },
+    enableBy: api.EnableBy.config,
+  });
+
+  api.addEntryCodeAhead(() => {
+    const { entryCodeAhead } = api.config.athena;
+    return entryCodeAhead;
+  });
+
+  api.addEntryImportsAhead(() => {
+    const { polyfill } = api.config.athena;
+    return polyfill ? [
+      {source: polyfill}
+    ]: [];
+  });
+}
